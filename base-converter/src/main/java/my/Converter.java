b8e0ca111341196bef5edc99e0edbae88052d1c0
Yosif Taibi <yosif.taibi@gmail.com>
2022-01-20 20:39:01 +0200

    Added comments about the conversion

--- base-converter/src/main/java/my/Converter.java
+++ base-converter/src/main/java/my/Converter.java
@@ -3,8 +3,8 @@ package my;
 import java.math.BigInteger;
 
 class Converter {
-    private static BigInteger myPow (long num , long pow){
-        BigInteger result = new BigInteger("1");
+    private static BigInteger myPow (long num , long pow){ // num to the power of pow
+        BigInteger result = new BigInteger("1"); 
 
         for(long i = 0 ; i < pow; i++)
             result = result.multiply(new BigInteger(Long.toString(num)));
@@ -14,7 +14,7 @@ class Converter {
 
     protected static Number convertToBase10 (Number num) {
         BigInteger result = new BigInteger("0");
-        String tempNumber = num.toString();
+        String tempNumber = num.toString(); 
         int power;
 
         if (num.isNegative()) {
@@ -22,7 +22,7 @@ class Converter {
             power = tempNumber.length() - 2; // Getting the last power
             for(int i = 1; i < tempNumber.length(); i++) {
                 BigInteger valueOfCurrChar = new BigInteger(Integer.toString(ValidCharacters.getValue(tempNumber.charAt(i)))); // The value of the current character
-                BigInteger toAdd = valueOfCurrChar.multiply(myPow(num.getRadix(),power)); // Formula
+                BigInteger toAdd = valueOfCurrChar.multiply(myPow(num.getRadix(),power)); // Formula: currentCharacterValue * (base^currentPower) 
                 result = result.add(toAdd); // Adding to the result
                 power--; // Update
             }
@@ -31,7 +31,7 @@ class Converter {
             power = tempNumber.length() - 1; // Getting the last power
             for(int i = 0; i < tempNumber.length(); i++) {
                 BigInteger valueOfCurrChar = new BigInteger(Integer.toString(ValidCharacters.getValue(tempNumber.charAt(i)))); // The value of the current character
-                BigInteger toAdd = valueOfCurrChar.multiply(myPow(num.getRadix(),power)); // Formula
+                BigInteger toAdd = valueOfCurrChar.multiply(myPow(num.getRadix(),power)); // Formula: currentCharacterValue * (base^currentPower)
                 result = result.add(toAdd); // Adding to the result
                 power--; // Update
             }
@@ -41,16 +41,16 @@ class Converter {
     }
 
     protected static Number fromDecimalTo (Number decimal , int targetBase) {
-        BigInteger tempDecimal = new BigInteger(decimal.toString());
+        BigInteger tempDecimal = new BigInteger(decimal.toString()); 
         StringBuilder result = new StringBuilder();
         int remainder;
 
         do{
-            remainder = tempDecimal.mod(BigInteger.valueOf(targetBase)).intValueExact();
-            result.append(ValidCharacters.getChar(remainder));
+            remainder = tempDecimal.mod(BigInteger.valueOf(targetBase)).intValueExact(); // getting the remainder of the division decimal/base
+            result.append(ValidCharacters.getChar(remainder));// adding the character with value=remainder  to the result
             tempDecimal = tempDecimal.divide(BigInteger.valueOf(targetBase));
-        }while(tempDecimal.signum() == 1);
+        }while(tempDecimal.signum() == 1); // repeat until the number is <=0
 
-        return new Number(result.reverse().toString(),targetBase);
+        return new Number(result.reverse().toString(),targetBase);// return result reversed
     }
 }
