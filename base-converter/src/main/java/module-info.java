9b29bba710064587616e503949deb2875846666c
Yosif Taibi <yosif.taibi@gmail.com>
2022-01-20 20:59:31 +0200

    Delete base-converter directory

--- base-converter/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module application {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    exports controllers;
-    opens controllers to javafx.fxml,javafx.controls;
-    exports application;
-    opens application to javafx.fxml;
-}
