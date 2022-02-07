7f0310d3612076a417d705ce29e6f3033a603a1a
Yosif Taibi <yosif1705@abv.bg>
2022-02-07 20:54:12 +0200

    Small changes

--- base-converter/src/main/java/my/Number.java
+++ base-converter/src/main/java/my/Number.java
@@ -1,7 +1,7 @@
 package my;
 
 public class Number {
-    private final String number;
+    private String number;
     private final int radix;
 
     private String processString(String str) {
@@ -24,21 +24,22 @@ public class Number {
     }
 
     private void isValid(String num , int base){
+        String invalidStringException = "For input string: \"" + num + "\"" + " under radix " + base;
+        String invalidRadixException = "For input radix: \"" + base + "\". " + "Radix out of bounds! Min = " + ValidCharacters.MIN_RADIX + " , Max = " +ValidCharacters.MAX_RADIX;
+
         for (int i = 0; i < num.length(); i++) {
-            String invalidStringException = "For input string: \"" + num + "\"" + " under radix " + base;
-            String invalidRadixException = "For input radix: \"" + base + "\". " + "Radix out of bounds! Min = " + ValidCharacters.MIN_RADIX + " , Max = " +ValidCharacters.MAX_RADIX;
             if (i == 0){
                 if(num.charAt(i) == '-'){
                     continue;
                 }
             }
-            if (!ValidCharacters.contains(num.charAt(i))){
+            if (!ValidCharacters.contains(num.charAt(i))) {
                 throw new NumberFormatException(invalidStringException);
             }
             if (ValidCharacters.getValue(num.charAt(i)) >= base) {
                 throw new NumberFormatException(invalidStringException);
             }
-            if(base < ValidCharacters.MIN_RADIX || base > ValidCharacters.MAX_RADIX){
+            if (base < ValidCharacters.MIN_RADIX || base > ValidCharacters.MAX_RADIX) {
                 throw new NumberFormatException(invalidRadixException);
             }
         }
@@ -60,11 +61,14 @@ public class Number {
     public boolean isNegative() {
         return this.number.charAt(0) == '-';
     }
+    private void setNumber (String num) {
+        this.number = processString(num);
+    }
 
     public Number (String num , int base) {
-        num = processString(num);
         isValid(num,base);
-        this.number = num;
+
+        setNumber(num);
         this.radix = base;
     }
 
